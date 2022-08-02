678e221b09973b1c82c2e1fe29d049a109a216c4
Len87G <Eleniac@github.com>
2022-08-02 09:37:25 -0600

    adjustments to timer function error still occuring

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,12 @@
 var getTime = document.querySelector ('#getTime');
 var timerElement = document.querySelector (".timer")
-function getTime() {
-    setInterval(){
-        var day = moment (). format ("dddd, MMMMM, Do, YYYY h:mm:ss a");
-        console.log (hello)
-        timerElement.innerHTML = day
+
+//day and time function
+
+function getTime () {
+    setInterval (function () {
+        var day = moment().format ("dddd, MMMMM, Do, YYYY h:mm:ss a");
+        console.log ("hello");
+        timerElement.innerHTML = day 
     }, 1000
-};
-getTime()
+)}; getTime ()
