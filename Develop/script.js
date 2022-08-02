113d6be644afc5dd423a8efde02b95c0dd93981c
Len87G <Eleniac@github.com>
2022-08-02 09:14:15 -0600

    linked javascript page to HTML

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,9 @@
 var getTime = document.querySelector ('#getTime');
 var timerElement = document.querySelector (".timer")
 function getTime() {
-    setInterval (function ()){
+    setInterval(){
         var day = moment (). format ("dddd, MMMMM, Do, YYYY h:mm:ss a");
+        console.log (hello)
         timerElement.innerHTML = day
     }, 1000
 };
