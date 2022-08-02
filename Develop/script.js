7a520f35e1e5a76480935dc4c28e91892f250a97
Len87G <Eleniac@github.com>
2022-08-02 13:44:04 -0600

    last commit did not work, timer moved to h2 header in HTML and removed get time variable in javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,10 @@
-var getTime = document.querySelector ('#getTime');
 var timerElement = document.querySelector (".timer")
 
 //day and time function
 
 function getTime () {
     setInterval (function () {
-        var day = moment().format ("dddd, MMMMM, Do, YYYY h:mm:ss a");
+        var day = moment().format ("dddd, MMMM, Do, YYYY h:mm:ss a");
         console.log ("hello");
         timerElement.innerHTML = day 
     }, 1000
