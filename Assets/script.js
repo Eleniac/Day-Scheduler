83af3c29e5e80f50956c57ce3b87c8c7effdfbc6
Len87G <Eleniac@github.com>
2022-08-06 14:57:20 -0600

    jquery to loop through colors and add a class

--- Assets/script.js
+++ Assets/script.js
@@ -23,34 +23,35 @@ function changeColor () {
 )}; changeColor ()  
 
 
-//loop over time blocks
-//check if I have moved past the time
+//9 hours
+
+
 var timeBlocks= ["8", "9", "10","11", "12", "1","2","3","4", "5"]
+$("textarea").each (function () {
+    $(this).addClass ("col-1");
+});
+if (getTime === col-10) {
+    $(timeBlocks).addClass(present,function (indexpresent)
+});
+
+// var past = document.querySelector (".past")
+// var present = document.querySelector (".present")
+// for (var i = 0; i < timeBlocks; i++) {
+//      + timeElement [i] + ".";
+// }
 
 
-var past = document.querySelector (".past")
-for (var i = 0; i < past; i++) {
-     + timeElement [i] + ".";
-}
-
-
-var present = document.querySelector (".present")
-for (var i = 0; i < timeBlocks; i++) {
-     + timeElement [i] + ".";
-}
-
-
-var future = document.querySelector (".future")
-for (var i = 0; i < future; i++) {
-     + timeElement [i] + ".";
-}
+// var future = document.querySelector (".future")
+// for (var i = 0; i < future; i++) {
+//      + timeElement [i] + ".";
+// }
 
 //if time has moved past current time turn time-block red
 //if time is in the future turn time-block green
 //if the time is the current time turn grey
 
 
-  
+
     function saveEvent () {
         document.getElementById ("saveBtn").innerHTML = "save";
         saveBtn.addEventListener("click",saveEvent)
